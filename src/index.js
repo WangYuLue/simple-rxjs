b107b5ec3a951ed3477234090bdc750b0d0371fb
wangyulue <1206180097@qq.com>
2020-03-17 14:28:56 +0800

    feat(*) test on codesandbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello rxjs')
+var http = require("http");
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    res.write("Hello Rxjs!"); //write a response to the client
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
